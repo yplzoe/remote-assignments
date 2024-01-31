16a6b4b009f5b56fa0a0fdfe37e8ed404f8cb215
yplzoe <103309763+yplzoe@users.noreply.github.com>
2024-01-31 15:03:22 +0800

    fix trackname

--- Week-3/Assignments/app.py
+++ Week-3/Assignments/app.py
@@ -2,63 +2,62 @@ from flask import Flask, request, render_template, url_for, redirect, make_respo
 
 app = Flask(__name__)
 
+
 @app.route("/data", methods=['GET'])
 def for_data():
     num = request.args.get('number')
     print(num)
-    if num==None:
+    if num == None:
         return "Lack of Parameter"
     elif num.isdigit():
-        num=int(num)
-        total_sum=int((1+num)*num/2)
+        num = int(num)
+        total_sum = int((1+num)*num/2)
         print(total_sum)
         return "{}".format(total_sum)
     else:
         return "Wrong Parameter"
 
+
 @app.route("/sum.html")
 def sum_of_data():
     return render_template("sum.html")
 
+
 @app.route("/myName")
 def get_name():
-    user=request.cookies.get('userID')
+    user = request.cookies.get('userID')
     print(user)
     if user != None:
         return "{}".format(user)
     else:
         return redirect('/trackName')
         # return "hello"
-    
-@app.route("/trackName", methods = ['POST', 'GET'])
+
+
+@app.route("/trackName", methods=['POST', 'GET'])
 def track_name():
     print(request.path)
-    user=request.args.get('name')
+    user = request.args.get('name')
     print(user)
     if user != None:
-        resp=make_response('hi')
-        resp.set_cookie('userID', user, path='/myName')
-        return resp
+        resp = make_response(redirect('/myName'))
+        resp.set_cookie('userID', user)
     else:
         resp = make_response(render_template("trackname.html"))
-    # resp.set_cookie('userID', user)
-    # resp.set_cookie('userID', 'aaaa')
-    # if user != None:
-    #     print(user)
-    #     resp.set_cookie('userID', 'wtf')
-    #     print(resp)
-    #     return redirect('/myName')
-    
-    # if request.method == 'GET':
-    #     resp.set_cookie('userID', user)
-    #     return "{}".format(user)
     return resp
+
+
+@app.route("/del")
+def del_cookie():
+    res = make_response('delete cookies')
+    res.delete_cookie('userID')
+    return res
+
 
 @app.route("/")
 def hello_world():
-	return "<p>Hello, World!</p>"
-
+    return "<p>Hello, World!</p>"
 
 
 if __name__ == '__main__':
-	app.run(debug=True, port=3000)
+    app.run(debug=True, port=3000)
