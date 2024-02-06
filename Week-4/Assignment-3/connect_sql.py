ac28ecb9e5fb725443264964fbbf607829636882
yplzoe <103309763+yplzoe@users.noreply.github.com>
2024-02-06 13:48:12 +0800

    update assignment 3

--- Week-4/Assignment-3/connect_sql.py
+++ Week-4/Assignment-3/connect_sql.py
@@ -1,49 +1,40 @@
 import pymysql.cursors
 
 # for database
+db = pymysql.connect(host='localhost',
+                     user='root',
+                     password='password',
+                     database='assignment',)
 
 
 def add_to_db(email, pwd):
-    db = pymysql.connect(host='localhost',
-                         user='root',
-                         password='password',
-                         database='assignment',)
-    with db:
-        with db.cursor() as cursor:
-            sql = "INSERT INTO `user` (`email`, `password`) VALUES (%s, %s)"
-            cursor.execute(sql, (email, pwd))
-        db.commit()
+    cursor = db.cursor()
+    sql = "INSERT INTO `user` (`email`, `password`) VALUES (%s, %s)"
+    cursor.execute(sql, (email, pwd))
+    db.commit()
 
 
 def check_db(email, pwd):
-    db = pymysql.connect(host='localhost',
-                         user='root',
-                         password='password',
-                         database='assignment',)
+
     all_correct = False
+    # wrong_email = False
     exist_email = False
-    with db:
-        with db.cursor() as cursor:
-            sql = "SELECT * FROM `user` WHERE `email`=%s"
-            cursor.execute(sql, (email,))
-            result = cursor.fetchone()
-            print(result)
-            print(type(result))
-            if result != None:
-                exist_email = True
-                if result[2] == pwd:
-                    all_correct = True
+    cursor = db.cursor()
+    sql = "SELECT * FROM `user` WHERE `email`=%s"
+    cursor.execute(sql, (email,))
+    result = cursor.fetchone()
+    print(result)
+    # print(type(result))
+    if result != None:
+        exist_email = True
+        if result[2] == pwd:
+            all_correct = True
 
     return all_correct, exist_email, result
 
 
 def delete_all():
-    db = pymysql.connect(host='localhost',
-                         user='root',
-                         password='password',
-                         database='assignment',)
-    with db:
-        with db.cursor() as cursor:
-            sql = "DELETE FROM `user`"
-            cursor.execute(sql)
-        db.commit()
+    cursor = db.cursor()
+    sql = "DELETE FROM `user`"
+    cursor.execute(sql)
+    db.commit()
