568e35b29b365a9e245f5290b8c94a793b9f5c94
yplzoe <103309763+yplzoe@users.noreply.github.com>
2024-02-20 15:19:13 +0800

    Update assignment_4.sql

--- Week-4/Assignment-4/assignment_4.sql
+++ Week-4/Assignment-4/assignment_4.sql
@@ -10,8 +10,7 @@ USE assignment;
 -- 1. select all articles with author's email
 SELECT * FROM article;
 -- 2. select articles from 7th to 12th sorted by id
-SELECT us.id, ar.* 
-FROM (SELECT * FROM article LIMIT 6, 6) AS ar
-INNER JOIN user as us
-ON ar.email=us.email
-ORDER BY us.id;
+SELECT us.id, ar.*
+FROM (SELECT * FROM user ORDER BY id LIMIT 6, 6) as us
+INNER JOIN article as ar
+ON ar.email=us.email;
